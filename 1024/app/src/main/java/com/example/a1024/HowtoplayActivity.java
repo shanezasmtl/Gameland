2f68d9fc9dbeb4caf86c1dcd9d06defad3857c48
nao-brinda <ngn.naomi@gmail.com>
2020-10-21 11:07:57 +0200

    Setting commit 21/10/2020

---
+++ 1024/app/src/main/java/com/example/a1024/HowtoplayActivity.java
@@ -0,0 +1,14 @@
+package com.example.a1024;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HowtoplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.howtoplay);
+    }
+}
